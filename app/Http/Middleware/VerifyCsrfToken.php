d3d634952396bf2ac51e9f7aaab31d1bc5788242
Gazi Monir-Uz-Zaman <ciphercall@gmail.com>
2024-10-27 18:48:53 +0600

    fixed login registration issues

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,6 +12,7 @@ class VerifyCsrfToken extends Middleware
      * @var array<int, string>
      */
     protected $except = [
-        //
+        'api/login',       // Add this line
+        'api/register',    // Add this line
     ];
 }
